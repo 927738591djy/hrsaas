2f8c5e2a30811b18d7155501cde2b0a52bca3fa4
djy <927738591@qq.com>
2022-05-28 20:42:15 +0800

    完成获取用户资料接口和token注入

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,13 +1,25 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
+import store from '@/store'
+
 const service = axios.create({
   // 如果执行 npm run dev  值为 /api 正确  /api 这个代理只是给开发环境配置的代理
   // 如果执行 npm run build 值为 /prod-api  没关系  运维应该在上线的时候 给你配置上 /prod-api的代理
   baseURL: process.env.VUE_APP_BASE_API, // 设置axios请求的基础的基础地址
   timeout: 5000 // 定义5秒超时
 }) // 创建一个axios的实例
-service.interceptors.request.use()
-// 响应拦截器
+
+// 请求拦截器
+service.interceptors.request.use(config => {
+  // 在这个位置需要统一的去注入token
+  if (store.getters.token) {
+    config.headers['Authorization'] = `Bearer ${store.getters.token}`
+  }
+  return config // 必须返回配置
+}, error => {
+  return Promise.reject(error)
+})
+
 service.interceptors.response.use(response => {
   // axios默认加了一层data
   const { success, message, data } = response.data
