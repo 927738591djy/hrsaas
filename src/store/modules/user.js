6bc3361b9d04cdfe958d5c25395eedbe66071d7e
djy <927738591@qq.com>
2022-05-28 21:08:12 +0800

    封装获取用户资料的action并共享用户状态

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,7 +1,8 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
-import { login } from '@/api/user'
+import { login, getUserInfo } from '@/api/user'
 const state = {
-  token: getToken() // 初始化vuex中的token就从缓存中获取
+  token: getToken(), // 初始化vuex中的token就从缓存中获取
+  userInfo: {} // 定义一个空的对象 不是null 因为后边我要开发userInfo的属性给别人用  userInfo.name
 }
 const mutations = {
   setToken(state, token) {
@@ -11,6 +12,14 @@ const mutations = {
   removeToken(state) {
     state.token = null // 先把vuex中的token清空
     removeToken() // 再调用removeToken把缓存中的token清空
+  },
+  // 设置用户信息
+  setUserInfo(state, result) {
+    state.userInfo = result
+  },
+  // 删除用户信息
+  removeUserInfo(state) {
+    state.userInfo = {}
   }
 }
 const actions = {
@@ -22,7 +31,14 @@ const actions = {
     // 现在有用户token
     // actions 修改state 必须通过mutations
     context.commit('setToken', result)
+  },
+
+  async getUserInfo(context) {
+    const result = await getUserInfo() // 获取返回值
+    context.commit('setUserInfo', result) // 将整个的个人信息设置到用户的vuex数据中
+    return result // 这里为什么要返回 为后面埋下伏笔
   }
+
 }
 export default {
   namespaced: true,
