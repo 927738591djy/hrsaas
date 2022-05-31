574ea66761bee3ecd48d491987a0c57d847d6d0f
djy <927738591@qq.com>
2022-05-31 16:51:46 +0800

    员工导入组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,12 @@
 // 该文件负责所有的公共的组件的全局注册
 import PageTools from '@/components/PageTools'
+import UploadExcel from '@/components/uploadExcel'
 
 export default {
   install(Vue) {
     // 注册全局的通用栏组件对象
     Vue.component('PageTools', PageTools)
+    Vue.component('UploadExcel', UploadExcel)
   }
+
 }
