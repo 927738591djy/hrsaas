1ec71cea9fbb4dcb36781dff5856fd469754b916
djy <927738591@qq.com>
2022-06-04 19:11:24 +0800

    mixin注入权限点方法

--- src/main.js
+++ src/main.js
@@ -11,13 +11,14 @@ import '@/styles/index.scss' // global css
 import App from './App'
 import store from './store'
 import router from './router'
-
+import checkPermission from './mixin/checkPermission'
 import '@/icons' // icon
 import '@/permission' // permission control
 import * as directives from '@/directives'
 import Component from '@/components'
 import * as filters from '@/filters'
 
+Vue.mixin(checkPermission)
 // 注册自定义指令
 // 遍历所有的导出的指令对象 完成自定义全局注册
 Object.keys(directives).forEach(key => {
