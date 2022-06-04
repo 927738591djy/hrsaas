002567ec4254bc2cef40835cddc5ee0c40a84ad8
djy <927738591@qq.com>
2022-06-04 18:32:35 +0800

    动态菜单，vuex控制路由

--- src/permission.js
+++ src/permission.js
@@ -17,8 +17,22 @@ router.beforeEach(async(to, from, next) => {
       // 只有放过去的时候才获取用户资料
       if (!store.getters.userId) {
         // 没有用户资料才要获取
-        await store.dispatch('user/getUserInfo')
         // 要等拿到用户资料再放行所以写await
+        const { roles } = await store.dispatch('user/getUserInfo')
+        // 如果说后续 需要根据用户资料获取数据的话 这里必须改成 同步
+        // 筛选用户的可用路由
+        // actions中函数 默认是Promise对象 调用这个对象 想要获取返回的值话 必须 加 await或者是then
+        // actions是做异步操作的
+        const routes = await store.dispatch('permission/filterRoutes', roles.menus)
+        // 添加完动态路由之后
+        // routes就是筛选得到的动态路由
+        // 动态路由 添加到 路由表中 默认的路由表 只有静态路由 没有动态路由
+        // addRoutes  必须 用 next(地址) 不能用next()
+        router.addRoutes([...routes, { path: '*', redirect: '/404', hidden: true }]) // 添加动态路由到路由表  铺路
+        next(to.path) // 相当于跳到对应的地址  相当于多做一次跳转 为什么要多做一次跳转
+        // 进门了，但是进门之后我要去的地方的路还没有铺好，直接走，掉坑里，多做一次跳转，再从门外往里进一次，跳转之前 把路铺好，再次进来的时候，路就铺好了
+      } else {
+        next()
       }
       next() // 直接放行
     }
