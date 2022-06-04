0b3de9898a69362bbb43cc0d6d3d43b5563af5d4
djy <927738591@qq.com>
2022-06-04 11:28:38 +0800

    新增编辑权限的弹层

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -18,6 +18,34 @@
           </template>
         </el-table-column>
       </el-table>
+
+      <!-- 弹出层 -->
+      <el-dialog :visible="showDialog" label-width="120px" title="新增编辑">
+        <el-form>
+          <el-form-item label="权限名称">
+            <el-input v-model="formData.name" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="权限标识">
+            <el-input v-model="formData.code" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="权限描述">
+            <el-input v-model="formData.description" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="开启">
+            <el-switch
+              v-model="formData.enVisible"
+              active-value="1"
+              inactive-value="0"
+            />
+          </el-form-item>
+        </el-form>
+        <el-row type="flex" justify="center">
+          <el-col :span="6">
+            <el-button size="small" type="primary">确定</el-button>
+            <el-button size="small">取消</el-button>
+          </el-col>
+        </el-row>
+      </el-dialog>
     </div>
   </div>
 </template>
@@ -29,7 +57,16 @@ import { tranListToTreeData } from '@/utils/index'
 export default {
   data() {
     return {
-      list: [] // 获取所有权限
+      list: [], // 获取所有权限
+      showDialog: true,
+      formData: {
+        name: '', // 名称
+        code: '', // 标识
+        description: '', // 描述
+        type: '', // 类型 该类型 不需要显示 因为点击添加的时候已经知道类型了
+        pid: '', // 因为做的是树 需要知道添加到哪个节点下了
+        enVisible: '0' // 开启
+      }
     }
   },
   created() {
