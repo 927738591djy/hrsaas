b43c9a2894ee988a36254d75a99b5c888d761a90
djy <927738591@qq.com>
2022-06-04 10:46:46 +0800

    新建权限点管理页面

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        权限管理
-      </h2>
+      <!-- 靠右按钮 -->
+      <PageTools>
+        <el-button slot="after" type="primary">添加权限</el-button>
+      </PageTools>
+      <!-- 表格 -->
+      <el-table border>
+        <el-table-column align="center" label="名称" />
+        <el-table-column align="center" label="标识" />
+        <el-table-column align="center" label="描述" />
+        <el-table-column align="center" label="操作">
+          <el-button type="text">添加</el-button>
+          <el-button type="text">编辑</el-button>
+          <el-button type="text">删除</el-button></el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
